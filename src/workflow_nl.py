282b794808d07ff7fc3a8a6a5a27b2e569e7788e
ZequnZ <zequn.zhou@n26.com>
2020-03-13 23:17:02 +0100

    update workflow

--- src/workflow_nl.py
+++ src/workflow_nl.py
@@ -1,11 +1,25 @@
 """Workflow to obtain and save the data"""
 
-from get_info_nl import get_coronavirus_info_nl, save_info_nl, RIVM_INFO_PAGE
+from get_info_nl import get_coronavirus_info_nl_v2, save_info_nl_v2, RIVM_INFO_PAGE
 
 if __name__ == "__main__":
-    csv_link, csv_update_date = get_coronavirus_info_nl(
+
+    # As the layout of the page is changed, the previous workflow
+    # doesn't work anymore (Noticed at 2020-03-12)
+    #################################################
+    #################################################
+    # csv_link, csv_update_date = get_coronavirus_info_nl(
+    #     RIVM_INFO_PAGE
+    #     + "/onderwerp/infectieziekten/regionaal-internationaal/coronavirus-covid-19#!node-coronavirus-covid-19-meldingen"
+    # )
+
+    # save_info_nl(csv_link, csv_update_date)
+    #################################################
+    #################################################
+
+    csv_str = get_coronavirus_info_nl_v2(
         RIVM_INFO_PAGE
         + "/onderwerp/infectieziekten/regionaal-internationaal/coronavirus-covid-19#!node-coronavirus-covid-19-meldingen"
     )
+    save_info_nl_v2(csv_str)
 
-    save_info_nl(csv_link, csv_update_date)
